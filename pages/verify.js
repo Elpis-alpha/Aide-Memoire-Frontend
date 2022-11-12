01cf316eae340f95c63a901ec934d999ec2144ce
Elpis-alpha <elpis409@gmail.com>
2022-11-12 08:48:49 +0100

    verify

--- pages/verify.js
+++ pages/verify.js
@@ -12,7 +12,7 @@ const verify = () => {
 
     <>
 
-      <HeadTag description={`Welcome to ${siteName}. We are a note keeping application, signup and keep your notes with us. Your notes can also be made public for others to view without access to your account" Login or sign up into ${siteName}`} crawl={true} />
+      <HeadTag title="Join us" description={`Welcome to ${siteName}. We are a note keeping application, signup and keep your notes with us. Your notes can also be made public for others to view without access to your account" Login or sign up into ${siteName}`} crawl={true} />
 
       <ProtectLinks />
 
